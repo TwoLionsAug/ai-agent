3675b25e9d358cff644fec6e0b576bf2e0a6ec57
huangguoce <huangguoce>
2025-04-25 15:50:24 +0800

    first commit

---
+++ src/main/java/com/ce/ceaiagent/CeAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ce.ceaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ce/ceaiagent/CeAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.ceaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
