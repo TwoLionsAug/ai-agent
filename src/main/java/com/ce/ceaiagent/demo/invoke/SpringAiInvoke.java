b7ad01e5d707f46fcea523fbcb0dfcff7b1d3c23
huangguoce <huangguoce>
2025-04-25 16:43:03 +0800

    qwenAi

---
+++ src/main/java/com/ce/ceaiagent/demo/invoke/SpringAiInvoke.java
@@ -0,0 +1,28 @@
+package com.ce.ceaiagent.demo.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author hgc
+ * @Date 2025/4/25/16:00
+ * @ClassName SpringAiInvoke
+ * @Description springAi框架调用ai
+ */
+@Component
+public class SpringAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashScopeChatModel;
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage assistantMessage = dashScopeChatModel.call(new Prompt("你好，我是世界上最帅的男人"))
+                .getResult()
+                .getOutput();
+        System.out.println(assistantMessage.getText());
+    }
+}
