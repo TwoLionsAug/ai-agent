20b624610cb0c69d6a2e0f2c4755e7c2a5d90b99
huangguoce <huangguoce>
2025-05-08 17:06:37 +0800

    documentloader

--- src/main/java/com/ce/ceaiagent/demo/invoke/SpringAiInvoke.java
+++ src/main/java/com/ce/ceaiagent/demo/invoke/SpringAiInvoke.java
@@ -1,5 +1,8 @@
 package com.ce.ceaiagent.demo.invoke;
 
+import com.ce.ceaiagent.exception.BusinessException;
+import com.ce.ceaiagent.exception.ErrorCode;
+import com.ce.ceaiagent.exception.ThrowUtils;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
@@ -23,6 +26,7 @@ public class SpringAiInvoke implements CommandLineRunner {
         AssistantMessage assistantMessage = dashScopeChatModel.call(new Prompt("你好，我是世界上最帅的男人"))
                 .getResult()
                 .getOutput();
+        //throw new BusinessException(ErrorCode.PARAMS_ERROR);
         System.out.println(assistantMessage.getText());
     }
 }
